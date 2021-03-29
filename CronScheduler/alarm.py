bd99d3bfc209b1da3fc44d6e3d989f6e94b033ac
BearPeee <47686185+constick@users.noreply.github.com>
2021-03-29 21:32:45 +0700

    Add files via upload

---
+++ CronScheduler/alarm.py
@@ -0,0 +1,127 @@
+from flask import Flask, render_template, redirect, flash, url_for, session
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from crontab import CronTab
+import datetime
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'tempKey'
+
+cron = CronTab(user='root')
+
+class setupForm(FlaskForm):
+	date = StringField('Date: ')
+	starttime = StringField('Begin: ')
+	endtime = StringField('End: ')
+	submit = SubmitField('Submit')
+
+def validatetime(hour, minute, endhour, endminute, day, month):
+	hour = int(hour)
+	minute = int(minute)
+	endhour = int(endhour)
+	endminute = int(endminute)
+	day = int(day)
+	month = int(month)
+	
+def formatdatetime(x):
+	if x < 10:
+		return f'0{x}'
+	else:
+		return str(x)
+
+def writetime(hour, minute, endhour, endminute, day, month, n):
+	hour = int(hour)
+	minute = int(minute)
+	endhour = int(endhour)
+	endminute = int(endminute)
+	day = int(day)
+	month = int(month)
+	if n == -5:
+		if minute >= 5:
+			minute -= 5
+		else:
+			hour -= 1
+			minute += 55
+	elif n == 5:
+		if minute < 55:
+			minute += 5
+		else:
+			hour += 1
+			minute -= 55
+	elif n == -10:
+		if endminute >= 10:
+			endminute -= 10
+		else:
+			endhour -= 1
+			endminute += 50
+			
+	path = 'omxplayer -p /home/pi/mp3/'
+	if n == 0:
+		path += 'B+00.mp3'
+	elif n == -1:
+		path += 'E+00.mp3'
+	elif n == -5:
+		path += 'B-05.mp3'
+	elif n == +5:
+		path += 'B+05.mp3'
+	else:
+		path += 'E-10.mp3'
+	
+	if n == 0:
+		s_day = formatdatetime(day)
+		s_month = formatdatetime(month)
+		s_hour = formatdatetime(hour)
+		s_minute = formatdatetime(minute)
+		s_endhour = formatdatetime(endhour)
+		s_endminute = formatdatetime(endminute)
+		job = cron.new(command = path, comment = f'{s_day}/{s_month} {s_hour}:{s_minute} {s_endhour}:{s_endminute}')
+	else:
+		job = cron.new(command = path, comment = 'notstart')
+	if n == -1 or n == -10:
+		job.hour.on(endhour)
+		job.minute.on(endminute)
+	else:
+		job.hour.on(hour)
+		job.minute.on(minute)
+	job.dom.on(day)
+	job.month.on(month)
+	cron.write()
+
+@app.route('/', methods=['GET','POST'])
+def index():
+	form = setupForm()
+	
+	tabledata = list()
+	
+	for job in cron:
+
+		temp = dict()
+		
+		if job.comment != 'notstart':
+			date, begin, end = job.comment.split()
+			temp['date'] = date
+			temp['begin'] = begin
+			temp['end'] = end
+			tabledata.append(temp)
+	
+	if form.validate_on_submit():
+		day, month = form.date.data.split('/')
+		beginhour, beginminute = form.starttime.data.split(':')
+		endhour, endminute = form.endtime.data.split(':')
+		
+		flash('Scheduled successfully')
+		
+		writetime(beginhour, beginminute, endhour, endminute, day, month, 0)
+		writetime(beginhour, beginminute, endhour, endminute, day, month, -5)
+		writetime(beginhour, beginminute, endhour, endminute, day, month, +5)
+		writetime(beginhour, beginminute, endhour, endminute, day, month, -1)
+		writetime(beginhour, beginminute, endhour, endminute, day, month, -10)
+		
+		return redirect(url_for('index'))
+		
+	return render_template('main.html', form=form, data=tabledata)
+
+if __name__ == '__main__':
+	app.run(port=80, host='0.0.0.0')
+
